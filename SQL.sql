8e6a3f3234b5491168c35d80381a1dafef4913f8
ivonaive <59066142+ivonaive@users.noreply.github.com>
2020-05-20 17:58:00 -0400

    “fixed

--- SQL.sql
+++ SQL.sql
@@ -5,7 +5,7 @@ SELECT * FROM Customers;
 SELECT DISTINCT country FROM Customers;
 
 3. Get all the records from the table Customers where the Customer’s ID starts with “BL”.
-SELECT * FROM Customers WHERE customer_id starts LIKE 'BL%';
+SELECT * FROM Customers WHERE customer_id LIKE 'BL%';
 
 4. Get the first 100 records of the orders table.
 SELECT * FROM orders ORDER BY order_date ASC LIMIT 100;
@@ -14,30 +14,29 @@ SELECT * FROM orders ORDER BY order_date ASC LIMIT 100;
 SELECT * FROM customers WHERE postal_code IN ('1010', '3012', '12209', '05023');
 
 6. Get all orders where the ShipRegion is not equal to NULL.
-SELECT * FROM orders WHERE NOT ShipRegion='null';
+SELECT * FROM orders WHERE NOT ship_region='null';
 
 7. Get all customers ordered by the country, then by the city.
 SELECT * FROM Customers ORDER BY country, city;
 
 8. Add a new customer to the customers table. You can use whatever values/
-INSERT INTO Customers (contact_name, city, postal_code, country) VALUES ('Ivona','Farmington Hills','48331','USA');
-
+INSERT INTO customers (customer_id, company_name, contact_name, contact_title, address, city, region, postal_code, country,phone,fax) VALUES ('12345576','MRM','Ivona','Cook','30450 Fox Club Ct','Farmington','Michigan','48331', 'USA','2483033737','2483033738');
 9. Update all ShipRegion to the value ‘EuroZone’ in the Orders table, where the
 ShipCountry is equal to France.
-UPDATE orders SET ShipRegion='EuroZone' WHERE ShipCountry='France';
+UPDATE orders SET ship_region='EuroZone' WHERE ship_country='France';
 
 10. Delete all orders from `order_details` that have a quantity of 1.
 DELETE FROM order_details WHERE quantity='1';
 
 11. Calculate the average, max, and min of the quantity at the `order details` table.
-SELECT AVG(quantity), MAX(quantity), MIN(quantity), FROM order_details
+SELECT AVG(quantity), MAX(quantity), MIN(quantity) FROM order_details
 
 12. Calculate the average, max, and min of the quantity at the `order details` table,
 grouped by the orderid.
-SELECT MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity, MAX(quantity) AS max_quantity FROM order_details GROUP BY orderid
+SELECT MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity, MAX(quantity) AS max_quantity FROM order_details GROUP BY order_details;
 
 13. Find the CustomerID that placed order 10290 (orders table)
-SELECT CustomerID FROM orders WHERE orderid='10290'
+SELECT customer_id FROM orders WHERE order_id='10290'
 
 14. Do an inner join, left join, right join on orders and customers tables. (These are three
 separate queries, one for each type of join.)
@@ -47,7 +46,7 @@ SELECT * FROM orders RIGHT JOIN Customers  ON orders.customer_id = customers.cus
 
 15. Use a join to get the ship city and ship country of all the orders which are associated
 with an employee who is in London.
-SELECT employees.city, employees.country INNER JOIN employees ON employees.city = 'London'
+SELECT employees.city, employees.country FROM Customers INNER JOIN employees ON employees.city = 'London';
 
 16. Use a join to get the ship name of all orders that include a discontinued product. (See
 orders, order_details, and products. 1 means discontinued.)
@@ -57,6 +56,6 @@ SELECT orders.ship_name, order_details.order_id, products.discontinued FROM ((or
 SELECT first_name FROM employees WHERE reports_to IS NULL;
 
 18. Get first names of all employees who report to Andrew.
-SELECT first_name FROM employees WHERE reports_to 'Andrew';
+SELECT employees.first_name, employees.last_name, superior.first_name = 'Andrew' FROM employees LEFT OUTER JOIN employees superior ON employees.reports_to = superior.employee_ID WHERE employees.reports_to IS NOT NULL
 
 
